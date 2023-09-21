8470f2da75351fc2979b9fbb74b2151166da7609
Fallflower <xy673636000@gmail.com>
2023-09-21 17:52:34 +0800

    MaxHeap添加了插入(上浮)

--- Chapter5/Heap/Heap.h
+++ Chapter5/Heap/Heap.h
@@ -1,12 +1,12 @@
 #ifndef __MAX_HEAP_H__
 #define __MAX_HEAP_H__
 #include "../../Assistance.h"
-
+#define MAX_SIZE 100
 template<class E>
 class MaxHeap
 {
 private:
-    E *data;
+    E data[MAX_SIZE+1];
     int length;
 
     void adjustHeap(int k) {
@@ -24,20 +24,29 @@ private:
         data[k] = data[0];
     }
 
+    void percolateUp(const int& holeIndex, const E& e) {
+        int hi = holeIndex;
+        int pi = holeIndex / 2;
+        while (hi > 1 && data[pi] < e)
+        {
+            data[hi] = data[pi];
+            hi = [pi];
+            pi /= 2;
+        }
+        data[hi] = e;
+    }
+
 public:
     /*
     所有非终端节点：编号 i <= floor(n / 2)
     */
-    MaxHeap(): data(NULL), length(0) {}
+    MaxHeap(): length(0) {}
     MaxHeap(const E A[], const int& n) {
         makeHeap(A, n);
     }
 
     void makeHeap(const E A[], const int& len) {
-        if (data != NULL) delete[] data;
-
         length = len;
-        data = new E[length + 1];
         for (int i = 0; i < length; i++)
             data[i+1] = A[i];
         for (int i = length/2; i > 0; i--) // 调整所有非终端结点
@@ -47,8 +56,9 @@ public:
         Display(data, length+1);
     }
 
-    ~MaxHeap() {
-        if (data != NULL) delete[] data;
+    void Insert(const E& e) {
+        if (length == MAX_SIZE) throw Error("Heap array OVERFLOW!");
+        percolateUp(++length, e);
     }
 };
 
